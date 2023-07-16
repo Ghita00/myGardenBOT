d7c5c8ec1a6619d3be7c5a7fc8b89570151bdc94
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-07-16 23:23:04 +0200

    added git ignore and structured project

--- projectCode/index.py
+++ projectCode/index.py
@@ -2,24 +2,34 @@
 # It echoes any incoming text messages.
 
 import telebot
+from credential import *
 
-API_TOKEN = "6082253199:AAGT1KpXmQRD2xttftf1kdI3eAeexQIMezA"
 bot = telebot.TeleBot(API_TOKEN)
 
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
 
+'''
+Command to implement
+- Create Garden -> /creaGiardino 
+- Update water -> /annaffiaOggi
+- Last day water -> /acquaIeri
+- Report water last week -> /reportIdrico
+'''
+
+# Handle '/help'
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, """I comandi offerti sono:\n/creaGiardino -> crea un tuo giardino.\n/annaffiaOggi -> aggiorna la fornitura d'acqua concessa oggi.\n/acquaIeri -> fornisce un prospetto di quali piante hanno ricevuto l'acqua ieri.\n/reportIdrico -> fornisce un prospetto rispetto alla fornitura d'acqua nell'ultima settimana.
+    """)
+
+# Handle '/start'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, """Ciao, sono MyGardenBOT e mi occupo di aiutarti con il tuo giardino :).\nCrea il tuo giardino con il comando /creaGiardino.\nPer tutti visualizzare tutti i comandi utilizza il comando /help""")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, "test della risposta")
-
 
 bot.infinity_polling()
