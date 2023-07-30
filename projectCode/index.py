bc2d98dbf480fbcb3856e966148b86533d51e81b
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-07-30 11:47:58 +0200

    completed business logic of bot

--- projectCode/index.py
+++ projectCode/index.py
@@ -15,10 +15,9 @@ bot = telebot.TeleBot(API_TOKEN)
 '''
 Command to implement
 - Create Garden -> /creaGiardino ok
-- Update water -> /annaffiaOggi
-- Last day water -> /acquaIeri
+- Update water -> /annaffiaOggi ok
+- Last day water -> /acquaIeri ok
 - Report water last week -> /reportIdrico
-ADD TO LIST
 - Check Garden -> /giardino ok
 '''
 
@@ -42,7 +41,7 @@ def updateGarden(newGarden):
 # Handle '/help'
 @bot.message_handler(commands=['help'])
 def helpCommand(message):
-    bot.reply_to(message, """I comandi offerti sono:\n/creaGiardino -> crea un tuo giardino.\n/annaffiaOggi -> aggiorna la fornitura d'acqua concessa oggi.\n/acquaIeri -> fornisce un prospetto di quali piante hanno ricevuto l'acqua ieri.\n/reportIdrico -> fornisce un prospetto rispetto alla fornitura d'acqua nell'ultima settimana.
+    bot.reply_to(message, """I comandi offerti sono:\n/creaGiardino -> crea un tuo giardino.\n/annaffiaOggi -> aggiorna la fornitura d'acqua concessa oggi.\n/acquaIeri -> fornisce un prospetto di quali piante hanno ricevuto l'acqua ieri.\n/reportIdrico -> fornisce un prospetto rispetto alla fornitura d'acqua nell'ultima settimana\n/giardino -> fornisce il recap del tuo giarino.
     """)
 
 # Handle '/start'
@@ -71,23 +70,50 @@ def welcomeCommand(message):
 @bot.message_handler(commands=['acquaIeri'])
 def lastDayWatherCommand(message):
 
-    yesterday = weekWater[(len(weekWater)-1)]
-    yesterday = yesterday.split('\n ')
-    text = "Annaffiate ieri:\n"
-    for i in range(1, len(yesterday)):
-        text += "- " + yesterday[i] + "\n"
-    bot.reply_to(message, text)
-
+    if(len(weekWater) > 1):
+        yesterday = weekWater[(len(weekWater)-2)]
+        yesterday = yesterday.split('\n ')
+        text = "Annaffiate ieri:\n"
+        for i in range(1, len(yesterday)):
+            text += "- " + yesterday[i] + "\n"
+        bot.reply_to(message, text)
+    else:
+        bot.reply_to(message, "Non sono presenti abbastanza annaffiature nel mio database")
 @bot.message_handler(commands=['annaffiaOggi'])
 def todayWatherCommand(message):
     if checkMyGarden() :
         bot.reply_to(message, "Bene, scrivi pure un messaggio OGGI seguito da un elenco puntato seguendo questa struttura:\n- <pianta>\nUn esempio è il seguente:\nOGGI\n- salvia\n- rosmarino")
     else:
         bot.reply_to(message, "Nel mio sistema non è presente nessun giarino registrato. Creane uno subito con il comando \creaGiardino")
+
 @bot.message_handler(commands=['reportIdrico'])
 def reportWatherCommand(message):
     print(weekWater)
-    bot.reply_to(message, "wip")
+
+    report = "Report Idrico\n"
+    mapOfPlants = {};
+
+    for day in weekWater:
+        informationsOfDay = day.split(',')
+        report += "in data " + informationsOfDay[0] + " sono state annaffiate:\n";
+        if(len(informationsOfDay) == 2):
+            plants = informationsOfDay[1].split('\n')
+            for i in range(1, len(plants)):
+                report += "-" + plants[i] + "\n"
+                if(mapOfPlants.get(plants[i])):
+                    mapOfPlants.update(plants[i], mapOfPlants.get(plants[i])+1)
+                else:
+                    mapOfPlants[plants[i]] = 1
+        report += '\n'
+
+    report += "Resoconto annaffiate:\n"
+    plants = list(mapOfPlants.keys())
+    print(plants)
+    for i in range(0, len(mapOfPlants)):
+        report += plants[i] + " " + str(mapOfPlants.get(plants[i])) + "\n"
+
+
+    bot.reply_to(message, report)
 
 
 ######### COMMAND BY MESSAGE #########
@@ -105,6 +131,7 @@ def storangeGarden(message):
         updateGarden(Garden(editMessage))
         bot.reply_to(message, "Registrazione completata")
 
+# Update water today
 @bot.message_handler(func=lambda message: checkWater(message.text))
 def storangeWater(message):
     if (checkMyGarden()):
